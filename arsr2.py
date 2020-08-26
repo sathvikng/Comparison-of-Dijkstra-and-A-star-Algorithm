1de3f5a33b3c87b34a0eec3c3c27149479fcb740
Sathvik N G <54283537+sathvikng@users.noreply.github.com>
2020-08-26 20:00:21 +0530

    journal code astar2

--- arsr2.py
+++ arsr2.py
@@ -100,7 +100,7 @@ def obstacle_space(x,y,r):
         c=1
     #rhombus    
     if (38*x- 7*y - 5830/r >= 0) and (38*x + 23*y - 8530/r <= 0) and (37*x -20*y -6101/r <= 0) and (37*x +10*y - 6551/r >= 0):
-   	c=1
+        c=1
     ##parallelogram
     if (2*x + 19*y - 2514/r <= 0) and (41*x+ 25*y -5300/r >= 0) and (y - 90/r>= 0) and (37*x +10*y - 5000/r <= 0):
         c=1
@@ -240,9 +240,8 @@ while(flag!=1):
                 c_nd.append(nd)
                 cst.append(round((cost+cst[x]),1))
                 h_nd.append(round((cost+cst[x]+astar(nd)),2))
-		scnd+=1
-
-            
+                scnd+=1
+    
     #North West
     nd,cost=up_left(ndx)
     if (nd[1]>=0 and nd[0]>=0 and obstacle_space(nd[0],nd[1],r)!=1):
@@ -264,7 +263,7 @@ while(flag!=1):
                 c_nd.append(nd)
                 cst.append(round((cost+cst[x]),1))
                 h_nd.append(round((cost+cst[x]+astar(nd)),2))
-		scnd+=1
+                scnd+=1
 
             
     #North East
@@ -288,9 +287,8 @@ while(flag!=1):
                 c_nd.append(nd)
                 cst.append(round((cost+cst[x]),1))
                 h_nd.append(round((cost+cst[x]+astar(nd)),2))
-		scnd+=1
-
-            
+                scnd+=1
+                
     #South West
     nd,cost=down_left(ndx)
     if (nd[1]<=rows and nd[0]>=0 and obstacle_space(nd[0],nd[1],r)!=1):
@@ -312,6 +310,7 @@ while(flag!=1):
                 c_nd.append(nd)
                 cst.append(round((cost+cst[x]),1))
                 h_nd.append(round((cost+cst[x]+astar(nd)),2))
+                scnd+=1
 
 
             
@@ -336,7 +335,7 @@ while(flag!=1):
                 c_nd.append(nd)
                 cst.append(round((cost+cst[x]),1))
                 h_nd.append(round((cost+cst[x]+astar(nd)),2))
-		scnd+=1
+                scnd+=1
 
             
     vp_nd.append(p_nd.pop(x))
